8e324d0b8bdbed9c10fe43b4a028f21924650acb
Abhishek Pokhrel <abhishekpokhrel001@gmail.com>
2025-06-16 18:33:56 +0545

    Created First script containing the variables.

---
+++ bash-scripting/variables/variables.sh
@@ -0,0 +1,17 @@
+#In this script I will be covering the topics related to variables in bash scripting.
+#Variables are used to store data in a script.
+#Variables are declared using the syntax: variable_name=value
+#Variables are used to store data in a script.
+
+echo "Let's start with the basics of variables in bash scripting."
+#Declaring variables
+name="Abhishek"
+age=21
+#Accessing variables    
+echo "My name is $name and I am $age years old."
+#Variables can be declared in different ways.
+#Internally and Externally. Internally declared variables are not exported to the environment 
+#while externally declared variables are exported to the environment. Above example is an internally declared variable.
+
+
+
