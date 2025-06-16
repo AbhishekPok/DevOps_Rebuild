e664f03b2b2f1bb66ec5d52992eb9e3ad360378c
Abhishek Pokhrel <abhishekpokhrel001@gmail.com>
2025-06-16 18:27:09 +0545

    added message stating the completion of creation of Directories.

--- bash-scripting/basic_scripting.sh
+++ bash-scripting/basic_scripting.sh
@@ -2,6 +2,7 @@
 echo "Hello, World!"
 #Since I am making this script executable. I am thinking of doing as much as automation as possible.
 echo "In upcoming times I will be using this script to automate my work. And cover topics related to bash scripting and programmming topics."
-echo "And Executing this script will make Files and Folders related to heading and topics related to Scripting."
+echo "And Executing this script will make Files and Directories related to heading and topics related to Scripting."
 
 mkdir variables loops conditionals functions arrays commmand_execution commmand_redirection file_handling error_handling
+echo "Files and Folders created successfully".
