26bffd0d8216cfdf656bae3ec8fa15c7d7298ac2
Abhishek Pokhrel <abhishekpokhrel001@gmail.com>
2025-06-16 18:23:20 +0545

    Created script stating objective on basic level and created folder according to the studing topics.

---
+++ bash-scripting/basic_scripting.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Hello, World!"
+#Since I am making this script executable. I am thinking of doing as much as automation as possible.
+echo "In upcoming times I will be using this script to automate my work. And cover topics related to bash scripting and programmming topics."
+echo "And Executing this script will make Files and Folders related to heading and topics related to Scripting."
+
+mkdir variables loops conditionals functions arrays commmand_execution commmand_redirection file_handling error_handling
